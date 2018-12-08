3e46097ad69c196220f6c57c3ecfbf23a535a71c
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-08 20:38:05 +0000

    Update Music.js

--- Music.js
+++ Music.js
@@ -273,4 +273,19 @@ client.on('message', message => {
   }
   });
 
+client.on('message', msg => {
+
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("521061895206338600").join(); 
+    });
+
 client.login(process.env.BOT_TOKEN);
